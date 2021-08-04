8963cc92c4a8f0c3703d53a46037245462781668
ACristoff <acristoffanini@gmail.com>
2021-08-04 20:42:53 +0000

    created a greeting function

---
+++ hello.js
@@ -0,0 +1,9 @@
+const sayHello  = function (name) { //sayhello variable, = anonymous function (paramater)
+  console.log("Hello, " + name);
+}
+
+
+sayHello();
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
